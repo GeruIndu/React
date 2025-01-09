3270216d8265bf60700adc1c2b9abe7bb388b586
GeruIndu <indrajitmondal284@gmail.com>
2025-01-10 00:44:24 +0530

    todo v-2

---
+++ 13-Todo-List-version-2/src/components/AddTodo.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function AddTodo( {onNewItem} ) {
+
+    const [todoName, setTodoName] = useState('');
+    const [todoDate, setTodoDate] = useState('');
+
+    const handleChangeName = (e) => {
+        setTodoName(e.target.value);
+    }
+
+    const handleChangeDate = (e) => {
+        setTodoDate(e.target.value)
+    }
+
+    const handleAddClick = () => {
+      onNewItem(todoName, todoDate);
+      setTodoDate('');
+      setTodoName('');
+    }
+
+    return (
+      <div className="container text-center">
+        <div className="row kg-row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here" 
+                onChange={handleChangeName}
+            />
+          </div>
+          <div className="col-4">
+            <input type="date" 
+                onChange={handleChangeDate}
+            />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-success kg-button"
+                onClick={handleAddClick}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default AddTodo;
---
+++ 13-Todo-List-version-2/src/components/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+
+function ErrorMessage({todoItems}) {
+
+    return <h2>Empty Bucket</h2>
+}
+
+export default ErrorMessage;
