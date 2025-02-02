e68704ee340e4e31f2a6585a4b41c0840388f4d5
GeruIndu <indrajitmondal284@gmail.com>
2025-02-02 09:19:20 +0530

    myntra clone start

---
+++ 26-Myntra Clone/FrontEnd/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './routes/App'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home'
+import Bag from './routes/Bag'
+
+const router = createBrowserRouter ([
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {path: '/', element: <Home />},
+        {path: '/bag', element: <Bag />}
+      ]
+    }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router = {router}/>
+  </StrictMode>,
+)
